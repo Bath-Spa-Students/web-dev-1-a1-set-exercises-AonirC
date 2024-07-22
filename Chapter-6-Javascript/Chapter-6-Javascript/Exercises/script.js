e9264ad08d98b5a2795c76c9e1b84f6ee8bc482e
AonirC <144213680+AonirC@users.noreply.github.com>
2024-07-22 22:56:55 +0400

    Chapter 6

---
+++ Chapter-6-Javascript/Chapter-6-Javascript/Exercises/script.js
@@ -0,0 +1,6 @@
+function fuelPrice(litres, pricePerLitre) {
+    for(i = 2; i <= 10; i <= 2)
+        if(litres <= i)
+            pricePerLitre -+ .05
+        return (litres * pricePerLitre).toFixed(1.72)
+}
